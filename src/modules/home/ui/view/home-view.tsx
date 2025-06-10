ab164d69c0001591f6fce6b217668c970e1c5592
Raj-venom <rajsingh.rsr9824@gmail.com>
2025-06-11 00:35:48 +0545

    feat: integrate TRPC and React Query for improved data fetching and state management

--- src/modules/home/ui/view/home-view.tsx
+++ src/modules/home/ui/view/home-view.tsx
@@ -1,50 +1,28 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
-import { useRouter } from 'next/navigation';
 
 export const HomeView = () => {
-    const {
-        data: session,
-        isPending, //loading state
-        error, //error object
-        refetch //refetch the session
-    } = authClient.useSession()
 
-    const router = useRouter();
-
-    if (isPending) {
-        return <div>Loading...</div>;
-    }
-
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
-
-    if (!isPending && !session) {
-        return (
-            <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-                <h1 className="text-2xl font-bold mb-4">You are not logged in</h1>
-            </div>
-        )
-    }
+    const trpc = useTRPC();
+    const { data } = useQuery(trpc.hello.queryOptions({ text: "raj" }));
 
 
 
     return (
         <div>
-            <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-                <h1 className="text-2xl font-bold mb-4">Welcome, {session?.user.name}!</h1>
-                <p className="mb-4">You are email: {session?.user.email}</p>
-                <Button onClick={() => authClient.signOut({
-                    fetchOptions: {
-                        onSuccess: () => router.push("/sign-in"),
-                        onError: (error) => console.error("Sign out error:", error)
-                    }
-                })}>Sign Out</Button>
-            </div>
+            {data?.greeting ? (
+                <h1 className="text-2xl font-bold">
+
+                    {data.greeting}
+                </h1>
+            ) : (
+                <h1 className="text-2xl font-bold">
+                    Loading...
+                </h1>
+            )}
 
         </div>
     )
