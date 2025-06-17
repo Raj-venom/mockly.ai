06b793dc0d4646432f97533205ae719f3545200d
Raj-venom <rajsingh.rsr9824@gmail.com>
2025-06-17 16:51:02 +0545

    feat: add responsive dialog component for mobile and desktop views, replacing CommandDialog in DashboardCommand

---
+++ src/components/responsive-dialog.tsx
@@ -0,0 +1,76 @@
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer"
+import { useIsMobile } from "@/hooks/use-mobile";
+
+
+interface ResponsiveDialogProps {
+    title: string;
+    description: string;
+    children: React.ReactNode;
+    isOpen: boolean;
+    onOpenChange: (open: boolean) => void;
+}
+
+export const ResponsiveDialog = ({
+    title,
+    description,
+    children,
+    isOpen,
+    onOpenChange,
+
+}: ResponsiveDialogProps) => {
+    const isMobile = useIsMobile();
+
+    if (isMobile) {
+        return (
+            <Drawer open={isOpen} onOpenChange={onOpenChange}>
+                <DrawerContent>
+                    <DrawerHeader>
+                        <DrawerTitle>{title}</DrawerTitle>
+                        <DrawerDescription>{description}</DrawerDescription>
+                    </DrawerHeader>
+                    <div className="p-4">
+                        {children}
+                    </div>
+                </DrawerContent>
+            </Drawer>
+        )
+    }
+
+    return (
+        <Dialog open={isOpen} onOpenChange={onOpenChange}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription>{description}</DialogDescription>
+                </DialogHeader>
+                {children}
+                <DialogFooter>
+                    {/* <DialogClose asChild>
+                        <button className="btn">Close</button>
+                    </DialogClose> */}
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+
+}
